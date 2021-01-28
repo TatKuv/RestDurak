792355a309f445f188bda411b30371973579b76d
Tatiana Kuvarzina <ttatiana.s.mails@gmail.com>
2021-01-28 23:36:20 +0300

    git add .

---
+++ src/main/resources/Cards.java
@@ -0,0 +1,23 @@
+package com.durak.durakapp.response;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Cards {
+    private String masti;
+    private String name;
+    private int rank;
+
+    public String getMasti() {
+        return(this.masti);
+    }
+
+    public String getName() {
+        return(this.name);
+    }
+
+    public int rank() {
+        return(this.rank);
+    }
+}

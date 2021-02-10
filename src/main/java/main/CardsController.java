05f000ddd6a8e27e681962baaa532067dd29da6a
Tatiana Kuvarzina <ttatiana.s.mails@gmail.com>
2021-02-11 01:53:40 +0300

    PostMapping

--- src/main/java/main/CardsController.java
+++ src/main/java/main/CardsController.java
@@ -1,8 +1,9 @@
 package main;
 
-import response.Card;
 import java.util.ArrayList;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,23 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@RequestMapping("/cards")
 public class CardsController {
-    @RequestMapping(value = "/cards", method = RequestMethod.POST)
-    @ResponseBody
-    public String editWinner( @RequestBody String pojo) {
-        System.out.print(pojo);
-        return "pysya";
+
+    @PostMapping(value = "/c", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+            MediaType.APPLICATION_JSON_VALUE })
+
+    public ResponseEntity ArrayList(@RequestBody ParamsFromFront paramsFromFront) {
+        // return "paramsFromFront";
+        if (paramsFromFront.getMethod().equals("start_game")) {
+            ArrayList<Card> cards = new ArrayList<>();
+            cards.add(new Card(2, "piki"));
+            cards.add(new Card(3, "piki"));
+            return new ResponseEntity(cards, HttpStatus.OK);
+        }
+
+        return null;
+
     }
-    
-    public ResponseEntity turn(@PathVariable int rank) {
 
-        return (
-            
-        );
-    }                                    //создать метод который возвращает карты
-                                        // Класс для хранения состояний, тек. колода??
-
+    // } //создать метод который возвращает карты
+    // Класс для хранения состояний, тек. колода??
 
 }
